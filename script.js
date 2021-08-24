e5363aaf0f5495641908f6b5e0b6ff9ae55c7742
Abdul-Salam Adedoja <54647672+abdulsalam-a@users.noreply.github.com>
2021-08-24 15:03:26 -0400

    Online stopwatch
    A stopwatch created using HTML, CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+    var minutes = 0;
+    var seconds = 00;
+    var tens = 00;
+    
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+    var appendMinutes = document.getElementById("minutes")
+    var buttonStart = document.getElementById('start');
+    var buttonStop = document.getElementById('stop');
+    var buttonReset = document.getElementById('reset');
+    var Interval ;
+
+
+   function startTimer () {
+        tens++; 
+        
+        if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+        }
+        
+        if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+        } 
+        
+        if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+        } else {
+        appendSeconds.innerHTML = "0" + seconds;
+        }
+
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+        }    
+        
+        if (seconds > 25){
+            minutes++;
+            appendMinutes.innerHTML = minutes;
+            seconds = "0" + 0;
+        }
+  
+    }
+
+    buttonStart.onclick = function () {
+        Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = function() {
+        clearInterval(Interval);
+    }
+
+    buttonReset.onclick = function() {
+        clearInterval(Interval);
+        minutes = "0";
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+        appendMinutes.innerHTML = minutes;
+    }
+}
